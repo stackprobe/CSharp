3fe0a714388267ddda8ba56652098a7f3f3af855
stackprobe <stackprobe@gmail.com>
2016-01-21 10:33:19 +0900

    a

--- Module/Module/Tools/BracketParser.cs
+++ Module/Module/Tools/BracketParser.cs
@@ -3,85 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Charlotte.Tools
+namespace Informatix.Tools
 {
 	public class BracketParser
 	{
-		public static string Perform(string text)
+		/// <summary>
+		/// $$ -> $
+		/// ${aaa}, $(bbb), $[ccc], etc.
+		/// </summary>
+		private const char E = '$';
+
+		public delegate string Filter_d(string text);
+
+		public class Bracket
 		{
-			Stack<BracketInfo> bis = new Stack<BracketInfo>();
+			/// <summary>
+			/// "{}", "()", "[]", etc.
+			/// </summary>
+			public string BrChrs;
+			public Filter_d D_Filter;
+
+			public Bracket(string b, Filter_d f)
+			{
+				BrChrs = b;
+				D_Filter = f;
+			}
+		}
+
+		private List<Bracket> Brackets = new List<Bracket>();
+
+		public void Add(Bracket bracket)
+		{
+			Brackets.Add(bracket);
+		}
+
+		private Stack<Info> Infos = new Stack<Info>();
+
+		private class Info
+		{
+			public Bracket Bracket;
+			public int LeftIndex; // 左括弧の位置
+		}
+
+		public string Perform(string text)
+		{
+			Infos.Clear();
 
 			for (int index = 0; index < text.Length; )
 			{
-				if (1 <= bis.Count && bis.Peek().R == text[index])
+				if (1 <= Infos.Count && Infos.Peek().Bracket.BrChrs[1] == text[index])
 				{
-					BracketInfo bi = bis.Pop();
-					int iti = bi.L + 1;
-					string innerText = text.Substring(iti, index - iti);
+					Info info = Infos.Pop();
+					string innerText = text.Substring(info.LeftIndex + 1, index - info.LeftIndex - 1);
 
-					innerText = Filter(innerText, text[bi.L]);
+					innerText = info.Bracket.D_Filter(innerText);
 
-					string t1 = text.Substring(0, bi.L) + innerText;
-					string t2 = text.Substring(index + 1);
+					string lead = text.Substring(0, info.LeftIndex) + innerText;
+					string trail = text.Substring(index + 1);
 
-					text = t1 + t2;
-					index = t1.Length;
+					text = lead + trail;
+					index = lead.Length;
 				}
 				else
 				{
-					if (text[index] == '$')
+					if (text[index] == E)
 					{
-						text = StringTools.Remove(text, index, 1);
+						text = text.Substring(0, index) + text.Substring(index + 1); // erase E
 
-						if (text[index] != '$')
+						foreach (Bracket b in Brackets)
 						{
-							bis.Push(GetBI(text, index));
+							if (b.BrChrs[0] == text[index])
+							{
+								Info info = new Info();
+
+								info.Bracket = b;
+								info.LeftIndex = index;
+
+								Infos.Push(info);
+								break;
+							}
 						}
 					}
 					index++;
 				}
 			}
 			return text;
-		}
-
-		private class BracketInfo
-		{
-			public int L;
-			public char R;
-		}
-
-		private static BracketInfo GetBI(string text, int index)
-		{
-			BracketInfo bi = new BracketInfo();
-
-			bi.L = index;
-
-			switch (text[index])
-			{
-				case '{': bi.R = '}'; break;
-				case '(': bi.R = ')'; break;
-				case '[': bi.R = ']'; break;
-
-				default:
-					throw new Exception("不明な左括弧 [" + text[index] + "]");
-			}
-			return bi;
-		}
-
-		private static string Filter(string text, char lb)
-		{
-			switch (lb)
-			{
-				case '{':
-					return text;
-
-				case '(':
-					return text;
-
-				case '[':
-					return text;
-			}
-			throw new Exception("不明な左括弧 [" + lb + "]");
 		}
 	}
 }
