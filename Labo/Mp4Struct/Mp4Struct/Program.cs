5bd86b9fd507407a18d66f904ddd3ecdee9cde23
stackprobe <stackprobe@gmail.com>
2016-04-05 00:45:33 +0900

    a

---
+++ Labo/Mp4Struct/Mp4Struct/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mp4Struct
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Main2(args);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+		}
+
+		private static void Main2(string[] args)
+		{
+			//
+		}
+	}
+}
