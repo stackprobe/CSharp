46722936c324fe2a79bbac04ad1d89c74cf082ea
stackprobe <stackprobe@gmail.com>
2016-04-16 05:27:14 +0900

    a

--- Satellite/Satellite/Satellite/Tools/ExtToContentType.cs
+++ Satellite/Satellite/Satellite/Tools/ExtToContentType.cs
@@ -229,6 +229,11 @@ namespace Charlotte.Satellite.Tools
 			"xhtml","application/xhtml+xml",
 			"xls",  "application/vnd.ms-excel",
 			"doc",  "application/msword",
+
+			// 2016.4.16 追加
+
+			"swf",  "application/x-shockwave-flash",
+			"webm", "video/webm",
 		};
 
 		public const string DEF_EXT = "bin";
