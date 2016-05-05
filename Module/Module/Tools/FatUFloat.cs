37ebb7952e2ccb9dc4a1c4f1df8351bc65c2f7e8
stackprobe <stackprobe@gmail.com>
2016-05-06 05:56:17 +0900

    b

--- Module/Module/Tools/FatUFloat.cs
+++ Module/Module/Tools/FatUFloat.cs
@@ -16,6 +16,30 @@ namespace Charlotte.Tools
 		private UInt64 _radix; // 2 ～ 2^64-1
 		private int _exponent; // 0 ～ IMAX
 
+		public FatUInt Value
+		{
+			get
+			{
+				return _value;
+			}
+		}
+
+		public UInt64 Radix
+		{
+			get
+			{
+				return _radix;
+			}
+		}
+
+		public int Exponent
+		{
+			get
+			{
+				return _exponent;
+			}
+		}
+
 		public FatUInt Rem = null;
 
 		public FatUFloat(FatUInt value, UInt64 radix = DEF_RADIX, int exponent = 0)
@@ -34,24 +58,54 @@ namespace Charlotte.Tools
 			return new FatUFloat(_value.GetClone(), _radix, _exponent);
 		}
 
+		private static void Synchronize(FatUFloat a, FatUFloat b, int basement = 0)
+		{
+			if (a == null) throw new ArgumentException();
+			if (b == null) throw new ArgumentException();
+			if (a.Radix != b.Radix) throw new ArgumentException("different radix");
+			if (basement < 0 || IntTools.IMAX < basement) throw new ArgumentException();
+
+			int e = a.Exponent - b.Exponent - basement;
+
+			if (e < -IntTools.IMAX || IntTools.IMAX < e) throw new ArgumentException();
+
+			if (e < 0)
+				a.Synchronize(-e);
+			else if (0 < e)
+				b.Synchronize(e);
+		}
+
+		private void Synchronize(int e)
+		{
+			_value = FatUInt.Mul(_value, FatUInt.Power(new FatUInt(_radix), e));
+			_exponent += e;
+		}
+
 		public static FatUFloat Add(FatUFloat a, FatUFloat b)
 		{
-			throw null; // TODO
+			Synchronize(a, b);
+			return new FatUFloat(FatUInt.Add(a.Value, b.Value), a.Radix, a.Exponent);
 		}
 
 		public static FatUFloat Red(FatUFloat a, FatUFloat b)
 		{
-			throw null; // TODO
+			Synchronize(a, b);
+			return new FatUFloat(FatUInt.Red(a.Value, b.Value), a.Radix, a.Exponent);
 		}
 
 		public static FatUFloat Mul(FatUFloat a, FatUFloat b)
 		{
-			throw null; // TODO
+			if (IntTools.IMAX < a.Exponent + b.Exponent) throw new ArgumentException();
+			return new FatUFloat(FatUInt.Mul(a.Value, b.Value), a.Radix, a.Exponent + b.Exponent);
 		}
 
 		public static FatUFloat Div(FatUFloat a, FatUFloat b, int basement = DEF_BASEMENT)
 		{
-			throw null; // TODO
+			Synchronize(a, b, basement);
+			FatUInt value = FatUInt.Div(a.Value, b.Value);
+			FatUFloat ret = new FatUFloat(value, a.Radix, basement);
+			ret.Rem = value.Rem;
+			return ret;
 		}
 
 		public static FatUInt Mod(FatUFloat a, FatUFloat b, int basement = DEF_BASEMENT)
@@ -61,7 +115,8 @@ namespace Charlotte.Tools
 
 		public static FatUFloat Power(FatUFloat a, int exponent)
 		{
-			throw null; // TODO
+			if (exponent < 0 || IntTools.IMAX / a.Exponent < exponent) throw new ArgumentException();
+			return new FatUFloat(FatUInt.Power(a.Value, exponent), a.Radix, a.Exponent * exponent);
 		}
 
 		public static FatUFloat Root(FatUFloat a, int exponent, int basement = DEF_BASEMENT)
