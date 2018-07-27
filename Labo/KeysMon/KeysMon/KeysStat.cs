dda42b3e7cd0ef39191d5b0df9dcc5d3a49c4c6a
stackprobe <stackprobes@gmail.com>
2018-07-27 23:50:54 +0900

    tea time

---
+++ Labo/KeysMon/KeysMon/KeysStat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KeysMon
+{
+	public class KeysStat
+	{
+		[DllImport("user32.dll")]
+		static extern short GetAsyncKeyState(Keys vKey); 
+
+		public static bool[] GetStat()
+		{
+			bool[] dest = new bool[256];
+
+			for (int vk = 0; vk <= 255; vk++)
+			{
+				dest[vk] = GetAsyncKeyState((Keys)vk) != 0;
+			}
+			return dest;
+		}
+	}
+}
---
+++ Labo/KeysMon/KeysMon/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace KeysMon
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool[] last;
+			bool[] stat = KeysStat.GetStat();
+
+			for (; ; )
+			{
+				last = stat;
+				stat = KeysStat.GetStat();
+
+				for (int vk = 0; vk <= 255; vk++)
+				{
+					if (last[vk] && stat[vk] == false)
+					{
+						Console.WriteLine("U " + vk); // up
+					}
+					else if (last[vk] == false && stat[vk])
+					{
+						Console.WriteLine("D " + vk); // down
+					}
+				}
+
+				Thread.Sleep(100);
+				//Thread.Sleep(2000); // GetAsyncKeyStateの間でキー押下・放すしても検出してくれる⇒間隔は長くてもok
+			}
+		}
+	}
+}
