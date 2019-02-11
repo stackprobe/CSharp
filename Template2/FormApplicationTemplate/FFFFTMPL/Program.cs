ac6bfbf030c48c4bd250ec39e37a27ea2f688872
stackprobe <stackprobes@gmail.com>
2019-02-12 00:47:19 +0900

    daily

--- Template2/FormApplicationTemplate/FFFFTMPL/Program.cs
+++ Template2/FormApplicationTemplate/FFFFTMPL/Program.cs
@@ -2,11 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using System.Threading;
-using Microsoft.Win32;
-using System.Text;
-using System.IO;
-using System.Reflection;
 using Charlotte.Tools;
 
 namespace Charlotte
