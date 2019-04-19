c263cfccb9485ed866b8c03e4bbbaf6e8ec1a08c
stackprobe <stackprobes@gmail.com>
2019-04-19 22:20:30 +0900

    coffee break

---
+++ Labo/ScrMusHook/Test01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Tools;
+using Charlotte.Tests.ScrMusHook;
+
+namespace Charlotte
+{
+	class Program
+	{
+		public const string APP_IDENT = "{89e57647-e4ad-4643-8bfb-f8f70be1d0d1}";
+		public const string APP_TITLE = "ScrMusHook";
+
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2, APP_IDENT, APP_TITLE);
+
+			Console.WriteLine("Press ENTER.");
+			Console.ReadLine();
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			new ScrMusHook0001Test().Test01(); // -- 0001
+		}
+	}
+}
---
+++ Labo/ScrMusHook/Test01/Tests/ScrMusHook/ScrMusHook0001Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.ScrMusHook;
+
+namespace Charlotte.Tests.ScrMusHook
+{
+	public class ScrMusHook0001Test // -- 0001
+	{
+		public void Test01()
+		{
+			string message = "ScrMusHook9999";
+
+			if (new ScrMusHook0001().Echo(message) != message)
+				throw null;
+
+			Console.WriteLine("OK!");
+		}
+	}
+}
