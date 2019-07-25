8df474ed051772a140d2c9205447ca2b4b8b5691
stackprobe <stackprobes@gmail.com>
2019-07-25 19:36:59 +0900

    Heart

---
+++ Paint/SimplePaint/_Release.bat
@@ -0,0 +1,9 @@
+C:\Factory\Tools\RDMD.exe /RC out
+
+COPY SimplePaint\SimplePaint\bin\Release\SimplePaint.exe out
+COPY SimplePaint\SimplePaint\bin\Release\Chocolate.dll out
+COPY SimplePaint\SimplePaint\bin\Release\Chocomint.dll out
+
+C:\Factory\SubTools\zip.exe /O out SimplePaint
+
+PAUSE
