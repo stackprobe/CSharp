b8b5d808cc47324c4e7e0193409dc486473d9748
stackprobe <stackprobes@gmail.com>
2019-08-15 09:34:52 +0900

    Twinkle

---
+++ Chocolate/Chocolate/Tools/BluffList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte.Tools
+{
+	public class BluffList<T>
+	{
+		public Func<int> GetCount = () => -1;
+		public Func<int, T> GetElement = index => default(T);
+		public Action<int, T> SetElement = (index, value) => { };
+
+		// <---- prm
+
+		public int Count
+		{
+			get
+			{
+				return this.GetCount();
+			}
+		}
+
+		public T this[int index]
+		{
+			get
+			{
+				return this.GetElement(index);
+			}
+
+			set
+			{
+				this.SetElement(index, value);
+			}
+		}
+
+		public IEnumerable<T> Iterate()
+		{
+			int count = this.GetCount();
+
+			for (int index = 0; index < count; index++)
+			{
+				yield return this.GetElement(index);
+			}
+		}
+	}
+}
---
+++ Chocolate/Chocolate/Tools/BluffListTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte.Tools
+{
+	public static class BluffListTools
+	{
+		public static BluffList<T> Create<T>(T[] entity_binding, T defval = default(T), int count = IntTools.IMAX)
+		{
+			T[] entity = entity_binding;
+
+			return new BluffList<T>()
+			{
+				GetCount = () => count,
+				GetElement = index =>
+				{
+					if (index < 0 || entity.Length <= index) // ? out of range
+					{
+						return defval;
+					}
+					return entity[index];
+				},
+				SetElement = null,
+			};
+		}
+	}
+}
