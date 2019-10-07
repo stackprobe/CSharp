edbbca004f518191bd4f06f376bd8de5392c8f7a
stackprobe <stackprobes@gmail.com>
2019-10-08 00:39:47 +0900

    Echo

---
+++ Chocolate/Chocolate/Tools/BluffListTools.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte.Tools
+{
+	public static class BluffListTools
+	{
+		public static BluffList<T> Create<T>(T[] entity)
+		{
+			return new BluffList<T>(entity);
+		}
+	}
+}
