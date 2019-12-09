443670bf45b47561b4c3def2e8425e2c4ad1057b
stackprobe <stackprobes@gmail.com>
2019-12-10 02:01:17 +0900

    Amour

---
+++ SerialComm/Client/Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Charlotte
+{
+	class Program
+	{
+		private static SerialPortTerminal SerialPortTerminal;
+
+		static void Main(string[] args)
+		{
+			SerialPortTerminal = new SerialPortTerminal(args);
+
+			// TODO
+		}
+	}
+}
---
+++ SerialComm/Client/Client/SerialPortTerminal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// ^ sync @ SerialComm_SerialPortTerminal
+
+namespace Charlotte
+{
+	// sync > @ SerialComm_SerialPortTerminal
+
+	public class SerialPortTerminal
+	{
+		public SerialPortTerminal(string[] args)
+		{
+			// TODO
+		}
+
+		// TODO
+	}
+
+	// < sync
+}
