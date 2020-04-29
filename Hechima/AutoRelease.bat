f280fe459ba314e9b8fda64324e4ec2f3d485bae
stackprobe <stackprobes@gmail.com>
2020-04-30 02:14:22 +0900

    Melody

--- Hechima/AutoRelease.bat
+++ Hechima/AutoRelease.bat
@@ -1,21 +1,19 @@
-IF NOT EXIST Hechima_is_here.sig GOTO END
 CLS
 rem リリースして qrum します。
 PAUSE
 
+CALL newcsrr
+
 CALL ff
 cx **
-
 CD /D %~dp0.
-
-IF NOT EXIST Hechima_is_here.sig GOTO END
 
 CALL qq
 cx **
 
 CALL _Release.bat /-P
 
-MOVE out\HechimaClient.zip S:\リリース物\.
+MOVE out\* S:\リリース物\.
 
 START "" /B /WAIT /DC:\home\bat syncRev
 
