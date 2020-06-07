ec8c5ec7ce8ed134412ddcc327a48ec108f01257
stackprobe <stackprobes@gmail.com>
2020-06-08 07:17:53 +0900

    Etoile

--- Chocolate/Chocolate/Tools/HandleDam.cs
+++ Chocolate/Chocolate/Tools/HandleDam.cs
@@ -7,6 +7,12 @@ namespace Charlotte.Tools
 {
 	public class HandleDam
 	{
+		/// <summary>
+		/// 終了時に hDam に Add した全てのハンドルを Dispose する。
+		/// </summary>
+		/// <typeparam name="T">routine の戻り値の型</typeparam>
+		/// <param name="routine">hDam を渡して実行する routine</param>
+		/// <returns>routine の戻り値</returns>
 		public static T Section_Get<T>(Func<HandleDam, T> routine)
 		{
 			HandleDam hDam = new HandleDam();
@@ -20,6 +26,10 @@ namespace Charlotte.Tools
 			}
 		}
 
+		/// <summary>
+		/// 終了時に hDam に Add した全てのハンドルを Dispose する。
+		/// </summary>
+		/// <param name="routine">hDam を渡して実行する処理</param>
 		public static void Section(Action<HandleDam> routine)
 		{
 			HandleDam hDam = new HandleDam();
@@ -33,6 +43,12 @@ namespace Charlotte.Tools
 			}
 		}
 
+		/// <summary>
+		/// 例外発生時のみ hDam に Add した全てのハンドルを Dispose する。
+		/// </summary>
+		/// <typeparam name="T">routine の戻り値</typeparam>
+		/// <param name="routine">hDam を渡して実行する routine</param>
+		/// <returns>routine の戻り値</returns>
 		public static T Transaction_Get<T>(Func<HandleDam, T> routine)
 		{
 			HandleDam hDam = new HandleDam();
@@ -47,6 +63,10 @@ namespace Charlotte.Tools
 			}
 		}
 
+		/// <summary>
+		/// 例外発生時のみ hDam に Add した全てのハンドルを Dispose する。
+		/// </summary>
+		/// <param name="routine">hDam を渡して実行する処理</param>
 		public static void Transaction(Action<HandleDam> routine)
 		{
 			HandleDam hDam = new HandleDam();
@@ -63,12 +83,22 @@ namespace Charlotte.Tools
 
 		private List<IDisposable> Handles = new List<IDisposable>();
 
+		/// <summary>
+		/// ハンドルを追加する。
+		/// </summary>
+		/// <typeparam name="T">ハンドルの型</typeparam>
+		/// <param name="handle">追加するハンドル</param>
+		/// <returns>追加したハンドル</returns>
 		public T Add<T>(T handle) where T : IDisposable
 		{
 			this.Handles.Add(handle);
 			return handle;
 		}
 
+		/// <summary>
+		/// 全てのハンドルを Dispose する。
+		/// 1つ以上の Dispose が例外を投げた場合、全ての Dispose を実行し終えた後で AggregateException にまとめて投げる。
+		/// </summary>
 		public void Burst()
 		{
 			ExceptionDam.Section(eDam =>
@@ -77,6 +107,10 @@ namespace Charlotte.Tools
 			});
 		}
 
+		/// <summary>
+		/// 全てのハンドルを Dispose する。
+		/// 全ての Dispose を実行し終えた後で e と Dispose が投げた例外を AggregateException にまとめて投げる。
+		/// </summary>
 		public void Burst(Exception e)
 		{
 			ExceptionDam.Section(eDam =>
