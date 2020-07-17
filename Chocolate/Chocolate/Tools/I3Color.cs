e93b0083b7dcf1256dac6965ac57c02f54cbfc9e
stackprobe <stackprobes@gmail.com>
2020-07-18 03:01:28 +0900

    Pine

--- Chocolate/Chocolate/Tools/I3Color.cs
+++ Chocolate/Chocolate/Tools/I3Color.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace Charlotte.Tools
 {
 	public struct I3Color
 	{
+		/// <summary>
+		/// 赤 0 ～ 255
+		/// </summary>
 		public int R;
+
+		/// <summary>
+		/// 緑 0 ～ 255
+		/// </summary>
 		public int G;
+
+		/// <summary>
+		/// 青 0 ～ 255
+		/// </summary>
 		public int B;
 
 		public I3Color(int r, int g, int b)
@@ -21,6 +33,16 @@ namespace Charlotte.Tools
 		public override string ToString()
 		{
 			return string.Format("{0:x2}{1:x2}{2:x2}", this.R, this.G, this.B);
+		}
+
+		public Color ToColor()
+		{
+			return Color.FromArgb(this.R, this.G, this.B);
+		}
+
+		public static I3Color FromColor(Color color)
+		{
+			return new I3Color(color.R, color.G, color.B);
 		}
 	}
 }
--- Chocolate/Chocolate/Tools/I4Color.cs
+++ Chocolate/Chocolate/Tools/I4Color.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace Charlotte.Tools
 {
 	public struct I4Color
 	{
+		/// <summary>
+		/// 赤 0 ～ 255
+		/// </summary>
 		public int R;
+
+		/// <summary>
+		/// 緑 0 ～ 255
+		/// </summary>
 		public int G;
+
+		/// <summary>
+		/// 青 0 ～ 255
+		/// </summary>
 		public int B;
+
+		/// <summary>
+		/// アルファ値 0 ～ 255
+		/// </summary>
 		public int A;
 
 		public I4Color(int r, int g, int b, int a)
@@ -23,6 +39,16 @@ namespace Charlotte.Tools
 		public override string ToString()
 		{
 			return string.Format("{0:x2}{1:x2}{2:x2}{3:x2}", this.R, this.G, this.B, this.A);
+		}
+
+		public Color ToColor()
+		{
+			return Color.FromArgb(this.A, this.R, this.G, this.B);
+		}
+
+		public static I4Color FromColor(Color color)
+		{
+			return new I4Color(color.R, color.G, color.B, color.A);
 		}
 	}
 }
