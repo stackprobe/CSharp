b71326b2d2cd0dcf5f13eb31daf6c91465023ee8
stackprobe <stackprobes@gmail.com>
2020-07-28 08:19:36 +0900

    Happy

---
+++ wb/t20200727_JP2ToBmp/JP2ToBmp/JP2ToBmp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using Charlotte.Tools;
+
+namespace Charlotte
+{
+	class Program
+	{
+		public const string APP_IDENT = "{91206565-133f-41f0-b4c5-195f90c3fda6}";
+		public const string APP_TITLE = "JP2ToBmp";
+
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2, APP_IDENT, APP_TITLE);
+
+#if DEBUG
+			//if (ProcMain.CUIError)
+			{
+				Console.WriteLine("Press ENTER.");
+				Console.ReadLine();
+			}
+#endif
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			MessageBox.Show(APP_TITLE); // ---- 0001
+		}
+	}
+}
